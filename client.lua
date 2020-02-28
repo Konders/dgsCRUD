b6660437fdc4eadcac30c86ddf4151b9294f1428
Konders <illyashykin@gmail.com>
2020-02-28 09:40:31 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,45 +10,19 @@
 
 --     Удаление пользователей с всплывающим окном подтверждения
 
-local charset = {}  do -- [0-9a-zA-Z]
-    for c = 48, 57  do table.insert(charset, string.char(c)) end
-    for c = 65, 90  do table.insert(charset, string.char(c)) end
-    for c = 97, 122 do table.insert(charset, string.char(c)) end
-end
-
-local function randomString(length)
-    if not length or length <= 0 then return '' end
-    math.randomseed(os.clock()^5)
-    return randomString(length - 1) .. charset[math.random(1, #charset)]
-end
-
-local columns = {"ID","FirstName","SecondName","Location"}
-
 loadstring(exports.dgs:dgsImportFunction())()
 dgsSetSystemFont("sf.ttf",9,false,"proof")
 
-local listOfUsers = {} 
---Генерация списка, на тот момент пока не реализована бд
-for i=0,math.random(1,100) do 
-    listOfUsers[i] = {}
-    for k,v in pairs(columns) do 
-        table.insert(listOfUsers[i], randomString(math.random(0,50)))
-    end
-end
 local window 
 addEvent("retrieveUsers",true)
-addEventHandler("retrieveUsers",localPlayer,function(list) 
-    listOfUsers = list
-    if window and dgsGetType(window.window) == "dgs-dxwindow" then window:updateGridList(listOfUsers) 
+addEventHandler("retrieveUsers",localPlayer,function(list,columns) 
+    if window and dgsGetType(window.window) == "dgs-dxwindow" then window:updateGridList(list) 
     else
-        window = UsersWindow:new(listOfUsers,columns)
+        window = UsersWindow:new(list,columns)
     end
 
 end)
 addCommandHandler("crud",function(cmd,player) 
-    -- triggerServerEvent("onClientInsertUser",root,localPlayer,"Vadymko","Alkovich","Selo")
-    -- triggerServerEvent("onClientUpdateUser",root,localPlayer,11,"Bakbarich","Mahkid","Jinjin")
-    -- triggerServerEvent("onClientDeleteUser",root,localPlayer,11)
     triggerServerEvent("onClientGetUsers",root,localPlayer)
 end)
 triggerServerEvent("onClientGetUsers",root,localPlayer)
